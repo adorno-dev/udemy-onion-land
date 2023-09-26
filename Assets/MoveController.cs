6013067dc496e81d1d6968cc48c652587b0ccf16
adorno-dev <adorno.dev@gmail.com>
2023-09-26 12:19:25 -0300

    Rev Controllers

--- Assets/MoveController.cs
+++ Assets/MoveController.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class MoveController : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator anim;
+
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
     private float xInput;
@@ -14,28 +16,35 @@ public class MoveController : MonoBehaviour
     [SerializeField] private float groundCheckRadius;
     private bool isGrounded;
 
+    private bool facingRight = true;
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
+        AnimationControllers();
         CollisionChecks();
+        FlipController();
 
         xInput = Input.GetAxisRaw("Horizontal");
 
         Movement();
 
         if (Input.GetKeyDown(KeyCode.Space))
-        {
             Jump();
-        }
+
     }
 
-    private void CollisionChecks()
+    private void AnimationControllers()
     {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+        anim.SetFloat("xVelocity", rb.velocity.x);
+        anim.SetFloat("yVelocity", rb.velocity.y);
+        anim.SetBool("isGrounded", isGrounded);
     }
 
     private void Jump()
@@ -49,7 +58,27 @@ public class MoveController : MonoBehaviour
         rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
     }
 
-    private void OnDrawGizmos() {
+    private void FlipController()
+    {
+        if (rb.velocity.x < 0 && facingRight)
+            Flip();
+        else if (rb.velocity.x > 0 && !facingRight)
+            Flip();
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    private void CollisionChecks()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    private void OnDrawGizmos()
+    {
         Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
